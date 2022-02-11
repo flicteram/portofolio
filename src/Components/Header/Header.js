3b3937c399c2ca1acf0c11ca95758c1231ab5ead
Flicter Alexandru <flicteralexandru@gmail.com>
2022-02-11 22:35:56 +0200

    Change hover effect on nav links

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,48 +7,27 @@ function Header({homeNav,projects,aboutNav}){
     const [isOpen,setIsOpen]=useState(false)
     const navigate = useNavigate()
 
-    function handleOpen(){
-        if(isOpen){
-            setIsOpen(false)
-        }
-        else{
-            setIsOpen(true)
-        }
-        
-    }
-
-    const styles = (theme)=>({
-        [theme.breakpoints.down('sm')]:{
-            display:'initial',
-            cursor:'pointer',
-            width:30,
-            height:30
-        },
-        [theme.breakpoints.up('sm')]:{
-            display:'none',
-            cursor:'pointer',
-        }
-    })
+    const handleOpen=()=>isOpen?setIsOpen(false):setIsOpen(true)
     return(
         <nav>
             <div className='navInner'>
             <p className='logo' onClick={()=>navigate('/')}>{'<alexdev/>'}</p>
             <ul>
-            <Link to={'/'}><li className={homeNav?'locationNav':'notLocationNav'}>Home</li></Link>
-            <Link to={'/projects'}><li className={projects?'locationNav':'notLocationNav'}>Projects</li></Link>
-            <Link to={'/about'}><li className={aboutNav?'locationNav':'notLocationNav'}>About</li></Link>
+                <Link to={'/'}><li className={homeNav?'locationNav':'notLocationNav'}>Home</li></Link>
+                <Link to={'/projects'}><li className={projects?'locationNav':'notLocationNav'}>Projects</li></Link>
+                <Link to={'/about'}><li className={aboutNav?'locationNav':'notLocationNav'}>About</li></Link>
             </ul>
 
-            <div className={isOpen?'hamburgerOpen':'hamburger'} onClick={()=>handleOpen()}>
+            <div className={isOpen?'hamburgerOpen':'hamburger'} onClick={handleOpen}>
                 <div className='bar1'></div>
                 <div className='bar2'></div>
                 <div className='bar3'></div>
             </div>
             
             <div className={isOpen?'navLowOpen':'navLowClosed'}>
-            <Link to={'/'} className='navItemLow'>Home</Link>
-            <Link to={'/projects'} className='navItemLow'>Projects</Link>
-            <Link to={'/about'} className='navItemLow'>About</Link>
+                <Link to={'/'} className='navItemLow'>Home</Link>
+                <Link to={'/projects'} className='navItemLow'>Projects</Link>
+                <Link to={'/about'} className='navItemLow'>About</Link>
             </div>
 
             </div>           
